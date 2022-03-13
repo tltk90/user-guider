92142a22c89793e6df6660949a0a0ffc5ed9ca2a
Tzahi Levi <thetzahilevi@gmail.com>
2022-03-13 16:38:21 +0200

    first draft

---
+++ src/index.js
@@ -0,0 +1,108 @@
+const ANIMATE_TIME = 500;
+
+export function guide(config) {
+    let configIndex = 0;
+    let configCount = config.length - 1;
+    const { guider, guiderText, buttons} = initializeElement();
+    const setButtonDisableState = () => {
+        if(configIndex === 0) {
+            buttons.children[0].disabled = true;
+        }
+        else {
+            buttons.children[0].disabled = false;
+        }
+        if(configIndex === configCount) {
+            buttons.children[1].disabled = true;
+        }
+        else {
+            buttons.children[1].disabled = false;
+        }
+    };
+    buttons.children[0].addEventListener('click', () => {
+        // prev press
+        configIndex--;
+        requestAnimationFrame( () => {
+            setButtonDisableState();
+            showGuide(config[configIndex], guider, guiderText);
+        });
+
+    });
+    buttons.children[1].addEventListener('click', () => {
+        // next press
+        configIndex++;
+        requestAnimationFrame(() => {
+            setButtonDisableState();
+            showGuide(config[configIndex], guider, guiderText);
+        })
+    });
+    setButtonDisableState();
+    showGuide(config[configIndex], guider, guiderText);
+
+}
+
+
+function initializeElement() {
+    const overlay = document.createElement('div');
+    const guider = document.createElement('div');
+    const guiderText = document.createElement('span');
+    const buttons = document.createElement('div');
+    const close = document.createElement('div');
+    buttons.innerHTML = '<button id="prev"> &lt; </button> <button id="next">&gt;</button>';
+    overlay.setAttribute('class', 'ug-overlay');
+    guider.setAttribute('class', 'ug-container');
+    buttons.setAttribute('class', 'ug-container-button');
+    close.setAttribute('class', 'ug-close-button');
+    close.addEventListener('click', removeContainer);
+    guider.appendChild(close);
+    guider.appendChild(guiderText);
+    guider.appendChild(buttons);
+
+    guider.style.opacity = '0';
+    document.body.appendChild(overlay);
+    overlay.appendChild(guider);
+
+    return { guider, guiderText, buttons};
+}
+
+function showGuide(c, guider, guiderText) {
+    const el = document.querySelector(c.element);
+    const rect = el.getBoundingClientRect();
+    const onTop = rect.y < document.body.clientHeight / 2;
+    const onLeft = rect.x < document.body.clientWidth / 2;
+    let classToBeAdded;
+    if(onTop) {
+        classToBeAdded = onLeft ? 'ug-bubble-top' : 'ug-bubble-top-right';
+    }
+    else {
+        classToBeAdded = onLeft ? 'ug-bubble-bottom-left': 'ug-bubble-bottom';
+    }
+    function animate() {
+        const transition = `opacity ${ANIMATE_TIME / 2}ms ease-in-out`;
+        const oldT = el.style.transition;
+        guider.style.transition = transition;
+        guider.style.opacity = '0';
+        setTimeout(() => {
+            clearBubbleClass(guider);
+            guider.classList.add(classToBeAdded);
+            guiderText.innerHTML = c.text;
+            const top = onTop ? rect.bottom : undefined;
+            const bottom = !onTop ? (document.body.clientHeight - rect.top) : undefined;
+            const left = onLeft ? rect.x - (rect.width / 2): rect.x + (rect.width / 2) - guider.clientWidth;
+            guider.style.top = top ? `${top}px` : '';
+            guider.style.bottom = bottom ? `${bottom}px` : '';
+            guider.style.left = `${left}px`;
+            guider.style.opacity = '1';
+            guider.style.transition = oldT;
+        }, ANIMATE_TIME);
+    }
+
+    animate();
+}
+
+function clearBubbleClass(el) {
+    el.classList.remove('ug-bubble-bottom', 'ug-bubble-top', 'ug-bubble-bottom-left', 'ug-bubble-top-right');
+}
+function removeContainer() {
+    const overlay = document.querySelector('.ug-overlay');
+    document.body.removeChild(overlay);
+}
